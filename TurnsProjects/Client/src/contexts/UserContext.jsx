405918ca6c2143dad3d36ef6093593c45b154e42
RomerX64 <tomiromera2903@gmail.com>
2024-12-03 18:43:06 -0300

    changed

--- TurnsProjects/Client/src/contexts/UserContext.jsx
+++ TurnsProjects/Client/src/contexts/UserContext.jsx
@@ -1,65 +1,47 @@
 import { createContext, useState } from "react";
-import { useNavigate  } from 'react-router-dom';
-
+import { useNavigate } from "react-router-dom";
 
 export const UserContext = createContext({
-    user: null,
-    where:'/',
-    GetUser:()=>{},
-    
+  user: null,
+  where: "/",
+  GetUser: () => {},
 });
 
 // eslint-disable-next-line react/prop-types
-export const UserProvider = ({children})=>{
-    const [user, setUser]= useState()
-    const navigate = useNavigate()
-    const [where,setWhere]= useState()
-    
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState();
+  const navigate = useNavigate();
+  const [where, setWhere] = useState();
 
-    const GetUser = async ({username , password})=>{
-        try {
-            const response = await fetch(`http://localhost:3000/users/`,{
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({username , password}), 
-            })
-            if(!response.ok){
-                alert(`ERROR: ${response.status} ${response.statusText}`)
-                throw new Error(`ERROR: ${response.status} ${response.statusText}`)
-            }
-                const usuario = await response.json()
-            setUser(usuario)
-            setWhere('/home')   
-            navigate('/home')
-        
-        } catch (error) {
-            alert(error.message)
-        }
+  const GetUser = async ({ username, password }) => {
+    try {
+      const response = await fetch(`http://localhost:3000/users/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+      if (!response.ok) {
+        alert(`ERROR: ${response.status} ${response.statusText}`);
+        throw new Error(`ERROR: ${response.status} ${response.statusText}`);
+      }
+      const usuario = await response.json();
+      setUser(usuario);
+      setWhere("/home");
+      navigate("/home");
+    } catch (error) {
+      alert(error.message);
     }
+  };
 
-    const value ={
-        user,
-        GetUser,
-        where,
-        setWhere,
-        setUser
+  const value = {
+    user,
+    GetUser,
+    where,
+    setWhere,
+    setUser,
+  };
 
-    }
-  
-    
-    return (
-       <UserContext.Provider value={value}>{children}</UserContext.Provider>
-    )
-
-}
-
-
-
-
-
-
-
-
-
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
